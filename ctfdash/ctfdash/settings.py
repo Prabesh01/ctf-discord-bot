7d85cc49c45b5b4b90e594512d36f9f9fdfde8a8
Prabesh01 <prabesh01@pm.me>
2026-07-04 11:36:33 +0545

    Caddyfile, staticfiles - production deployment

--- ctfdash/ctfdash/settings.py
+++ ctfdash/ctfdash/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1v77zfkedoi&d-(mduh&o@72byboteqqmue^vcin7a8o0fbx+*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','conan.prax.to']
 CSRF_TRUSTED_ORIGINS = ['https://conan.prax.to']
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,7 @@ set -e
 python3 -u bot.py >  /var/log/bot.log 2>&1 &
 
 cd ctfdash
+
+python3 manage.py collectstatic --noinput
+
 exec gunicorn --bind 0.0.0.0:5050 ctfdash.wsgi:application
